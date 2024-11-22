1ac802239521111cf9742fe13ce595968dc7a202
Daniel Donahue <BCF12551@tempworks.com>
2024-11-22 11:42:39 -0600

    EXTANT.

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,4 +1,3 @@
-"use strict";
 /*
  * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
@@ -19,7 +18,18 @@ exports.modules = {
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n// Used to override internal Next.js file, by way of the underscore naming convention. \n// The internal Next.js file looks exactly like this anyway.\n// Use this file for adding global *.css styles.\n\nfunction App({ Component, props }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...props\n    }, void 0, false, {\n        fileName: \"C:\\\\ODonnchadha\\\\react-18-next-js-playbook\\\\pages\\\\_app.js\",\n        lineNumber: 5,\n        columnNumber: 12\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLHVGQUF1RjtBQUN2Riw0REFBNEQ7QUFDNUQsZ0RBQWdEOztBQUNoRCxTQUFTQSxJQUFJLEVBQUVDLFNBQVMsRUFBRUMsS0FBSyxFQUFDO0lBQzVCLHFCQUFPLDhEQUFDRDtRQUFXLEdBQUdDLEtBQUs7Ozs7OztBQUMvQjtBQUNBLGlFQUFlRixHQUFHQSxFQUFDIiwic291cmNlcyI6WyJDOlxcT0Rvbm5jaGFkaGFcXHJlYWN0LTE4LW5leHQtanMtcGxheWJvb2tcXHBhZ2VzXFxfYXBwLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIFVzZWQgdG8gb3ZlcnJpZGUgaW50ZXJuYWwgTmV4dC5qcyBmaWxlLCBieSB3YXkgb2YgdGhlIHVuZGVyc2NvcmUgbmFtaW5nIGNvbnZlbnRpb24uIFxyXG4vLyBUaGUgaW50ZXJuYWwgTmV4dC5qcyBmaWxlIGxvb2tzIGV4YWN0bHkgbGlrZSB0aGlzIGFueXdheS5cclxuLy8gVXNlIHRoaXMgZmlsZSBmb3IgYWRkaW5nIGdsb2JhbCAqLmNzcyBzdHlsZXMuXHJcbmZ1bmN0aW9uIEFwcCh7IENvbXBvbmVudCwgcHJvcHN9KSB7XHJcbiAgICByZXR1cm4gPENvbXBvbmVudCB7Li4ucHJvcHN9IC8+XHJcbn1cclxuZXhwb3J0IGRlZmF1bHQgQXBwOyJdLCJuYW1lcyI6WyJBcHAiLCJDb21wb25lbnQiLCJwcm9wcyJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/global.css */ \"./styles/global.css\");\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_global_css__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction App({ Component, props }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...props\n    }, void 0, false, {\n        fileName: \"C:\\\\ODonnchadha\\\\react-18-next-js-playbook\\\\pages\\\\_app.js\",\n        lineNumber: 3,\n        columnNumber: 12\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUE4QjtBQUM5QixTQUFTQSxJQUFJLEVBQUVDLFNBQVMsRUFBRUMsS0FBSyxFQUFDO0lBQzVCLHFCQUFPLDhEQUFDRDtRQUFXLEdBQUdDLEtBQUs7Ozs7OztBQUMvQjtBQUNBLGlFQUFlRixHQUFHQSxFQUFDIiwic291cmNlcyI6WyJDOlxcT0Rvbm5jaGFkaGFcXHJlYWN0LTE4LW5leHQtanMtcGxheWJvb2tcXHBhZ2VzXFxfYXBwLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi4vc3R5bGVzL2dsb2JhbC5jc3MnO1xyXG5mdW5jdGlvbiBBcHAoeyBDb21wb25lbnQsIHByb3BzfSkge1xyXG4gICAgcmV0dXJuIDxDb21wb25lbnQgey4uLnByb3BzfSAvPlxyXG59XHJcbmV4cG9ydCBkZWZhdWx0IEFwcDsiXSwibmFtZXMiOlsiQXBwIiwiQ29tcG9uZW50IiwicHJvcHMiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles/global.css":
+/*!***************************!*\
+  !*** ./styles/global.css ***!
+  \***************************/
+/***/ (() => {
+
+
 
 /***/ }),
 
@@ -29,6 +39,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \****************************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react/jsx-dev-runtime");
 
 /***/ })
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-// Used to override internal Next.js file, by way of the underscore naming convention. 
-// The internal Next.js file looks exactly like this anyway.
-// Use this file for adding global *.css styles.
+import '../styles/global.css';
 function App({ Component, props}) {
     return <Component {...props} />
 }
